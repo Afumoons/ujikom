9ab216f1281ccbd739048eb2e773d3ad8ac0eb1f
Afumoons <afumoons@gmail.com>
2022-05-23 08:48:50 +0700

    User Controller

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\StoreUserRequest;
+use App\Http\Requests\Admin\UpdateUserRequest;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::get();
+        return view('admin.user.index', [
+            'title' => 'Dashboard Pengguna',
+            'users' => $users,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $user_roles = User::USER_ROLES;
+        return view('admin.user.create', [
+            'title' => 'Tambah Pengguna',
+            'user_roles' => $user_roles,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        User::create($validatedData);
+
+        return redirect()->route('admin.user.index')->with('success', 'User has been created.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($user)
+    {
+        $user_roles = User::USER_ROLES;
+        $user = User::findOrFail($user);
+        return view('admin.user.edit', [
+            'title' => 'Atur Pengguna',
+            'user' => $user,
+            'user_roles' => $user_roles,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateUserRequest $request, $user)
+    {
+        $user = User::findOrFail($user);
+        $validatedData = $request->validated();
+
+        $user->update($validatedData);
+
+        return redirect()->route('admin.user.edit', $user->id)->with('success', 'User has been edited.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($user)
+    {
+        $user = User::findOrFail($user);
+
+        //delete user
+        $user->delete();
+
+        return redirect()->back()->with('success', 'User has been deleted.');
+    }
+}
