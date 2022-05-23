28b7d45b6d93f512cc45d8509a545eaf04580e4b
Afumoons <afumoons@gmail.com>
2022-05-23 08:49:26 +0700

    Move admin's controller to admin folder

--- app/Http/Controllers/Admin/ProductCategoryController.php
+++ app/Http/Controllers/Admin/ProductCategoryController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Models\ProductCategory;
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreProductCategoryRequest;
 use App\Http\Requests\UpdateProductCategoryRequest;
 
