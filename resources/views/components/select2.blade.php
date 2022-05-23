9297655f1564bc836f5ff73c47ea7f92e16a2ba7
Afumoons <afumoons@gmail.com>
2022-05-23 08:47:04 +0700

    create select2 component

---
+++ resources/views/components/select2.blade.php
@@ -0,0 +1,7 @@
+@push('css')
+    <link href="{{ URL::asset('/assets/libs/select2/select2.min.css') }}" rel="stylesheet" type="text/css" />
+@endpush
+@push('script')
+    <script src="{{ URL::asset('/assets/libs/select2/select2.min.js') }}"></script>
+    <script src="{{ URL::asset('/assets/js/pages/form-advanced.init.js') }}"></script>
+@endpush
