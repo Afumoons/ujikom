044cc4b866f4b0563dcaeff5cdc17ab874043ef2
Afumoons <afumoons@gmail.com>
2022-05-23 12:54:10 +0700

    Model Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\ProductCategory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'product_category_id',
+        'name',
+        'description',
+        'slug',
+        'price',
+        'image',
+        'stock',
+    ];
+
+    public function productCategory(): BelongsTo
+    {
+        return $this->belongsTo(ProductCategory::class);
+    }
 }
